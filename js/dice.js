6f7ef8b93b1c3e7c11a871c47aa27528520cec1b
Luigi Fontana <luigifontana92@gmail.com>
2020-02-19 17:30:49 +0100

    Chiusura Esercizio Dice
    Ho messo i due file JS sotto commento in HTML così da poter scegliere quale attivare

--- js/dice.js
+++ js/dice.js
@@ -6,9 +6,17 @@ chi fa di più vince
 // Creare variabile per ottenere numeri random
 // Confrontare i due numeri
 // Determinare il più alto
+document.getElementById('es-dice').setAttribute('class', 'visible');
 
 var dice1 = Math.floor(Math.random() * 6) + 1;
-console.log(dice1);
-
+document.getElementById('dice-one').innerHTML = dice1;
 var dice2 = Math.floor(Math.random() * 6) + 1;
-console.log(dice2);
+document.getElementById('dice-two').innerHTML = dice2;
+
+if (dice1 > dice2) {
+    document.getElementById('result').innerHTML = 'Player 1 Wins!';
+} else if (dice1 == dice2) {
+    document.getElementById('result').innerHTML = 'It\'s a Draw!';
+} else {
+    document.getElementById('result').innerHTML = 'Player 2 Wins!';
+}
