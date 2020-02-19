f8f4c8bdf82e12480d3d6b39657d37b5cb9d55e9
Luigi Fontana <luigifontana92@gmail.com>
2020-02-19 15:45:27 +0100

    Svolgimento esercizio mail

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-alert('prova');
+/*
+Chiedi all’utente la sua email
+Controlla che sia nella lista di chi può accedere
+Stampa un messaggio appropriato
+*/
+
+// Costruzione della lista di mail accettate
+// Prompt per inserimento mail
+// Messaggio di benvenuto
+
+var validMail = [
+    'mario@rossi.it',
+    'luca@verdi.it',
+    'matteo@bianchi.it',
+    'marco@gialli.it',
+    'silvia@galli.it',
+    'lucia@corti.it'
+];
+
+var inputMail = prompt('Inserisci la tua mail per accedere');
+for (var i = 0; i < validMail.length; i++) {
+    if (inputMail != validMail[i]) {
+        console.log('Benvenuto ' + inputMail);
+    }
+}
