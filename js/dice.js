ace53a517dd774bb572552adfa8fcc4079d8472b
Luigi Fontana <luigifontana92@gmail.com>
2020-02-19 16:47:11 +0100

    Inizio esercizio Dice

---
+++ js/dice.js
@@ -0,0 +1,14 @@
+/*
+Gioco dei Dadi
+chi fa di più vince
+*/
+
+// Creare variabile per ottenere numeri random
+// Confrontare i due numeri
+// Determinare il più alto
+
+var dice1 = Math.floor(Math.random() * 6) + 1;
+console.log(dice1);
+
+var dice2 = Math.floor(Math.random() * 6) + 1;
+console.log(dice2);
